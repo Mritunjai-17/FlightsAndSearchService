bbcce692575f66692034c3314b39cd0f1cf7bf6e
Mritunjai-17 <mritunjaiupadhyay885452@gamil.com>
2026-06-02 18:29:33 +0530

    Setup basic dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-    
+const express = require('express');
+
+const { PORT } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+
+    //create express obejct
+    const app = express();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+
+}
+
+setupAndStartServer();
