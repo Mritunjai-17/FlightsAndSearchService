19f6063594127f985c333c35ed8f89961736634d
Mritunjai-17 <mritunjaiupadhyay885452@gamil.com>
2026-06-08 17:50:18 +0530

    Added unique constraint and city repository

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
@@ -15,5 +16,4 @@ const setupAndStartServer = async () => {
     });
 
 }
-
 setupAndStartServer();
