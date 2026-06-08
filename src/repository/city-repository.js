47347b2c364647fc2263b1f3dec21f0de3e5f066
Mritunjai-17 <mritunjaiupadhyay885452@gamil.com>
2026-06-08 19:03:25 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,12 +1,15 @@
-const { City } = require('../models');
+const { City } = require('../models/index');
 
 class CityRepository {
     async createCity({ name }) {
         try {
-            const city = await City.create({ name });
+            const city = await City.create({ 
+                name : name 
+            });
             return city;
         } catch (error) {            
-            throw error;
+            console.log("Something went wrong in the repository layer");
+            throw { error };
         }
     }
 
@@ -18,7 +21,32 @@ class CityRepository {
                 }
         });
         } catch (error) {
-            throw error;
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: { 
+                    id: cityId 
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+     }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
         }
     }
 }
