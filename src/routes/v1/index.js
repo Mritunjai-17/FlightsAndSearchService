80894489131ea42dddcb49fc4d733feed396dcd9
Mritunjai-17 <mritunjaiupadhyay885452@gamil.com>
2026-06-09 16:20:28 +0530

    Added more routers for city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,8 @@ const controller = require('../../controllers/city-controller');
 const router = express.Router();
 
 router.post('/city', controller.create);
+router.delete('/city/:id', controller.destroy);
+router.get('/city/:id', controller.get);
+router.patch('/city/:id', controller.update);
 
 module.exports = router;
